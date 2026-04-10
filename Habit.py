b94537371d616207c7fcc38665af731a8b518d55
vinhc06 <vinhchauva@gmail.com>
2026-04-10 16:38:15 -0400

    habit.py import

--- Habit.py
+++ Habit.py
@@ -1,7 +1,11 @@
+import uuid
+from datetime import datetime
+
 class Habit:
-    def __init__(self, name, time, category, description):
+    def __init__(self, name, time, category_id, description, user_id):
         self.name = str(name)
         self.time = time
-        self.category = category
+        self.category_id = category_id
         self.description = description
-
+        self.user_id = user_id
+        
