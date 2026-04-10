7e995768777001e17de246776c222efc509a7f19
vedanthshiv <vedanthshiv@vt.edu>
2026-04-10 15:59:27 -0400

    Basic Habit Strcuture

---
+++ Habit.py
@@ -0,0 +1,7 @@
+class Habit:
+    def __init__(self, name, time, category, description):
+        self.name = name
+        self.time = time
+        self.category = category
+        self.description = description
+
