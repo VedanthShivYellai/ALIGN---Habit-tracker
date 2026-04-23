1e61073ba9d180c15414ca0a32a26e9d5e04d6ac
VedanthShivYellai <vsyellai@gmail.com>
2026-04-23 17:49:19 -0400

    Database Changes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, render_template, jsonify
 from Habit import Habit
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("serviceAccountKey.json")
+align = firebase_admin.initialize_app(cred, name="ALIGN")
+db = firestore.client(app=align)
 
 # This code had no AI use on it
 def add_habit():
@@ -13,14 +14,19 @@ def add_habit():
             data.get("user_id"),
             data.get("name"),
             data.get("category_id"),
-            data.get("goal_time", "any"),
-            data.get("recurrence", "daily"),
+            data.get("goal_time"),
+            data.get("recurrence"),
             data.get("alerts", []),
             data.get("current_streak", 0))
+    db.collection("User_Habits").document(habit.name).set(habit.getHabit())
     
 
 def remove_habit():
     data = request.json
+    
+    user_ID = data.get("user_id")
+
+    
     
 
 
